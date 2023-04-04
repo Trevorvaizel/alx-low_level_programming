b8adf3bde44dc199a7e47594d9da30f9b7c8fb26
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-04 08:13:41 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _islower - prints lower
+ * 
+ * Return: 1 if lower
+ *
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	if ( c >= 'a') && (c <= 'z')
+		return (1);
+	else
+		return (0);
+}
