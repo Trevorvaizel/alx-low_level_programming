be46d8974d1f2bcb83f97327dc2ab3439237f512
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-04 10:41:18 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - last digit printer
+ *@n: the input
+ * Return: 0
+ *
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit = (n % 10);
+
+	return (last_digit);
+}
