7d71050c85fb50c85ad2d06ce1006ca3d8f75db6
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-09 07:50:01 -0700

    Question_0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - resets numbers
+ * 
+ * Return: void
+ * 
+*/
+
+void reset_to_98(int *n)
+{
+    /* change what the pointer adress contains*/
+    *n = 98;
+
+    return (void);
+}
