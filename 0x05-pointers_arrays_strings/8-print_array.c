98d6e8dd126903c2d68590e660042cff7e2d58f9
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-11 14:25:38 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints
+ * @a: pointer to array
+ * @n: array size
+ *
+ * Return: void
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	if (i == 0)
+		printf("%d",*(a + i));
+	for (i = i + 1; i < n; i++)
+	{
+		printf(", %d", *(a + i));
+	}
+}
