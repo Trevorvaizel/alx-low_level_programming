1f4ba7226cc3704673b2fd35fe5378b13563736b
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-11 14:39:44 -0700

    0x05

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,10 +14,10 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	if (i == 0)
-		printf("%d", *(a + i));
+	printf("%d", *(a + i));
 	for (i = i + 1; i < n; i++)
 	{
 		printf(", %d", *(a + i));
 	}
+	printf("\n");
 }
