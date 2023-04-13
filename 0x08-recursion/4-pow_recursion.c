343ab89307d47c89d7436e6eb5c7baafa751e5db
Trevorvaizel <trevor.vaizelstudio@gmail.com>
2023-04-13 17:15:00 +0300

    0x08

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculates to factorial
+ * @x: parameter
+ * @y: parameter
+ *
+ * Return:0, -1
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * x);
+		y--;
+	}
+}
