545a3c0f38139c414f923ffe90e4d53bded2036a
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-13 05:17:28 -0700

    0x08

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: parameter
+ *
+ * Return: 0
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (
