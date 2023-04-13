50a64e8c32297ddd512336d8b4e1ed92ca2ab537
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-13 04:40:08 -0700

    0x08

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -15,6 +15,6 @@ void _print_rev_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
-	_print_rev_recursion(*s);
-	_print_rev_recursion(*(s + 1));
+	_print_rev_recursion(char *s);
+	_print_rev_recursion(char *(s + 1));
 }
