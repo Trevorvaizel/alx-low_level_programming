6d0c73b796b15459679fe72601e1a370e5679c81
Trevorvaizel <trevor.vaizelstudio@gmail.com>
2023-04-13 17:37:47 +0300

    0x08

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion(int n);
+ * @n: integer
+ *
+ * Return: -1,0
+ *
+ */
+int _sqrt_recursion(int n)
+{
+	int odd;
+
+	odd = 1;
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if(n == 0)
+	{
+		return n + _sqrt_recursion(n - odd);
+		odd = odd -2;
+	}
+}
