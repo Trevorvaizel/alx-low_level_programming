13e7ee472870f453fff6cab0f3fc2f5db34ad383
Trevorvaizel <trevor.vaizelstudio@gmail.com>
2023-04-13 17:07:00 +0300

    0x08

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,38 +1,14 @@
 #include "main.h"
-#include <string.h>
-
-/**
- * _print_rev_recursion - prints string in reverse
- * @s: parameter
- *
- * Return: 0
- *
- */
-
-void _print_rev_recursion(char *s)
-{
-	int len;
-
-	len = strlen(s) - 1;
 
 /**
 *_print_rev_recursion - Preint string in reverse
 *@s: Pointer to string
 */
-void _print_rev_recursion(char *s, len)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-};
-	/** this terminates the code */
-	if (len < 0)
-		return;
-	/**this prints the last string */
-	_putchar(*(s + len));
-	len--;
-	/** here its the adress thats changing */
-	_print_rev_recursion( s + (len - 1));
 }
