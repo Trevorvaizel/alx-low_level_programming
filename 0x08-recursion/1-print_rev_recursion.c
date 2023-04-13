67aa1ed2ec44918b999aa42e95fa493129d7951f
Trevorvaizel <trevor.vaizelstudio@gmail.com>
2023-04-13 15:51:35 +0300

    0x08

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -20,5 +20,5 @@ void _print_rev_recursion(char *s)
 	/**this prints the last string */
 	_putchar(*(s + len));
 	/** here its the adress thats changing */
-	_print_rev_recursion( s + (len - 1);
+	_print_rev_recursion( s + (len - 1));
 }
