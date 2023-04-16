6647d9b1ec395e9781a5629a84eb5c20e2fc51af
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-15 22:23:24 -0700

    0x05

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,24 +1,41 @@
 #include "main.h"
+/**
+*squareRoot - Calculate Square Root
+*@initial: 2
+*@end: Param 2
+*@m: Value to find sqrt
+*
+*Return: Squareroot of m
+*/
+
+int squareRoot(int initial, int end, int m)
+{
+	int middle;
+
+	if (end >= initial)
+	{
+		middle = initial + (end - initial) / 2;
+		if (middle * middle == m)
+			return (middle);
+		if (middle * middle > m)
+			return (squareRoot(initial, middle - 1, m));
+		if (middle * middle < m)
+			return (squareRoot(middle + 1, end, m));
+	}
+	return (-1);
+}
 
 /**
- * _sqrt_recursion(int n);
- * @n: integer
- *
- * Return: -1,0
- *
- */
+*_sqrt_recursion - Get squareroot using recursion
+*@n: Number to obtain sqrt
+*
+*Return: Squareroot
+*/
 int _sqrt_recursion(int n)
 {
-	int odd;
-
-	odd = 1;
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if(n == 0)
-	{
-		return (n + _sqrt_recursion(n - odd));
-	}
-	odd = odd - 2;
+	if (n == 0 || n == 1)
+		return (n);
+	return (squareRoot(2, n, n));
 }
