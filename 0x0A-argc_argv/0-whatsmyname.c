4dfcc3714c70ba434aef8ca1a4872e02a03c90f8
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-17 07:54:17 -0700

    0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: 0
+ *
+ */
+
+int main (int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s", *argv);
+
+	return (0);
+}
