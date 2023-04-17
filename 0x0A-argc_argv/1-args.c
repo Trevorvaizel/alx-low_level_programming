10c6e2736f3cb525df708d924e747437edaab999
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-17 08:04:47 -0700

    0x0A

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- prints no. of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%d\n", argc);
+
+	return (0);
+}
