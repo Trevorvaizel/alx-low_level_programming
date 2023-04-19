8ae294c0d633cebffe97792b25f1db41c083014c
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-19 08:50:53 -0700

    0x0C

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -12,11 +12,11 @@ void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
-	ptr = malloc(sizeof(int) * b);
+	ptr = malloc(b);
 	if (ptr == NULL)
 	{
 		exit(98);
 	}
 
-	return ((void *)ptr);
+	return (ptr);
 }
