318cbeab044d00e48fe7d22208c1694c628e61f7
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-19 07:05:42 -0700

    0x0B

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * alloc_grid - allocate grid
+ * @width: parameter 1
+ * @height: parameter 2
+ *
+ * Return: array
+ */
+int **alloc_grid(int width, int height)
+{
+	int w;
+	int h;
+	int **ptr;
+	int count;
+
+	if (width <= 0 && height <= 0)
+		return (NULL);
+	/** assign memory to arrays */
+	count = width * height;
+	ptr = malloc(sizeof(int) * count);
+	/** check for malloc */
+	if (ptr == NULL)
+		return (NULL);
+	h = 0;
+	while (h < height)
+	{
+		w = 0;
+		while (w < width)
+		{
+			**(ptr + (width * count + height)) = 0;
+			width++;
+		}
+		**(ptr + (width * count + height)) = 0;
+		h++;
+	}
+	return (ptr);
+}
