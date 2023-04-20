03dac07d4673a416eac41dfb555f81dea772b6ac
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-20 08:09:24 -0700

    0x0E

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,13 +7,16 @@
  * @d: dog
  * @name: name
  * @age: age
- * @owner
+ * @owner: owner
  *
  * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
