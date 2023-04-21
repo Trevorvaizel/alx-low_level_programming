fd6bf47802ee29ef41b4a1607519db9cec12e8f5
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-21 09:23:40 -0700

    0x0F

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,45 +4,14 @@
 #include "_putchar.c"
 
 /**
- * f  - prints individual characters
- * @char: input
+ * print_name - function prints name
+ * @name: pointer to a name
+ * @f: pointer to function
  *
  * Return: void
  */
 
-void (*f)(char*)
+void print_name(char *name, void (*f)(char *))
 {
-	*(f) = &_putchar;
-	f();
-}
-
-/**
- * print_name - function prints name
- * @f: pointer to putchar function
- *
- * Return: void 
- */
-
-void print_name(char *name, void (*f)(char*))
-{
-	int len;
-	int index;
-
-	f = &_putchar;
-	/** get the legnth of name **/
-	len  = strlen(name) + 1;
-	/** initialize name **/
-	name = malloc(sizeof(char) * len);
-	index = 0;
-	if (name != NULL)
-	{
-		for (index = 0; index < len; index++)
-		{
-			f(*(name + index));
-		}
-		f("\0");
-		f("\n");
-	}
-	else 
-		return(NULL);
+	f(name);
 }
