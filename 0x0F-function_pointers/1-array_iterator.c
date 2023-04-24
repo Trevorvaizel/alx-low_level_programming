4fbd605293d952745d246d875f9434fbb545c984
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-04-24 07:53:39 -0700

    0x0F

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_iterator - array itratot
+ * @array: pointer to array
+ * @size: array size
+ * @action: action intended
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	/** initialize the array */
+	array = malloc(sizeof(int) * size);
+	i = 0;
+	for (i = 0; i < size; i++)
+	{
+		action(*(a + i));
+	}
+}
