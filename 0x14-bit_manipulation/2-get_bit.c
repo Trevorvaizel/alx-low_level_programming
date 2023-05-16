4a0e6e3b04c7b64d528f4c4378dd5a9230a4df77
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-05-16 07:06:05 -0700

    0x14

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -32,4 +32,60 @@ unsigned long int get_size(int x)
  * get_binary - gets the binary of a number 
  * @x: passed parameter
  *
- * 
+ * Return: bin
+ */
+
+unsigned long int* get_bin(int x)
+{
+	unsigned long int *bin;
+	unsigned long int sum = get_size(x);
+	unsigned long int i;
+
+	/** assign malloc **/
+	bin = (unsigned long int*) malloc(sizeof(unsigned long int) * sum);
+	/**test for malloc */
+	if (bin == NULL)
+	{
+		return (NULL);
+	}
+	/** create bin array **/
+	for (;x > 1 ;)
+	{
+		for (i = 0; i < sum; i++)
+		{
+			if (x % 2 == 1)
+			{
+				x = ((x - 1) / 2);
+				bin[i] = 1;
+			}
+			else
+			{
+				x = x / 2;
+				bin[i] = 0;
+			}
+		}
+	}
+	return (bin);
+}
+
+/**
+ * get_bit - gets bit of a particular given index
+ * @n: provided parameter
+ * @index: index
+ *
+ * Return: index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int i;
+	int value;
+
+	/** get pointer to binary **/
+	bin = get_bin(n);
+	value = bin[index];
+
+	return (value);
+}
+
+
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
+unsigned long int* get_bin(int x);
+unsigned long int get_size(int x);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
