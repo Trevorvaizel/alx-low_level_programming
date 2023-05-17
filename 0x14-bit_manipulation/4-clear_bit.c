5d5f23df6bd1258b7c908034876d7b979758fc94
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-05-17 00:06:03 -0700

    0x14

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * clear_bit - clears bit
+ * @n: pointer to number
+ *
+ * Return: 1 if its worked
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+
+	return (*n &= (~(1UL << index))) ? 1 : -1;
+}
