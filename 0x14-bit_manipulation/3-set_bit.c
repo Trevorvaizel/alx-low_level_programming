a7a64cee333bd1e7f5bfc2144e1970c926710179
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-05-16 23:22:24 -0700

    0x14

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - changes bit at a particular place
+ * @n: pointer to int 
+ * @index: position to set bit
+ *
+ * Return: 1 if successful
+ *
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	return ((*n |= 1 << index) ? 1 : -1);
+}	
