3cda54f9a8611d02d839d4c23f941f682dec5d09
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-05-18 05:59:06 -0700

    0x15

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,10 +13,16 @@ int create_file(const char *filename, char *text_content)
 {
 	ssize_t fileDescriptor;
 	ssize_t bytesWritten;
+	ssize_t contentLegnth = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		contentLegnth = strlen(text_content);
 	}
 
 	fileDescriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
@@ -24,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	bytesWritten = write(fileDescriptor, text_content, strlen(text_content));
+	bytesWritten = write(fileDescriptor, text_content, contentLegnth);
 	if (bytesWritten == -1)
 	{
 		return (-1);
