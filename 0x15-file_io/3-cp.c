8b22a974ec65b7484b56c2b2a2d9c7e7ca0b3660
Trevorvaizel <trevorqueen.tq@gmail.com>
2023-05-21 04:16:32 -0700

    0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,84 +1,109 @@
-#include "main.h"
+#inlcude "main.h"
 
 /**
- * main - checks for errors in program then executes
- * @argc: argument count
+ * main: checks for errors and then executes
+ * @argc: argument counts
  * @argv: pointer to argument vector
  *
  * Return: 0 on success, -1 on faliure
  *
  */
 
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-    int fileDescriptor;
-    ssize_t bytesRead, bytesWritten;
-    ssize_t contentLength = 0;
-    char *buffer;
+	if(argc != 3)
+	{
+		fprintf(stderr, "Usage: cp file_from file to");
+		return (97);
+	}
 
-    if (argc != 3)
-    {
-        fprintf(stderr, "Error: Usage: cp file_from file_to\n");
-        return 97;
-    }
+	if(argv[1] == NULL)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
+	}
 
-    if (argv[1] == NULL)
-    {
-        fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-        return 98;
-    }
+	read_func(arg[1]);
+	write_func(arg[1], arg[2]);
 
-    fileDescriptor = open(argv[1], O_RDONLY);
-    if (fileDescriptor == -1)
-    {
-        fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-        return 98;
-    }
+	return (0);
+}
 
-    if (argv[1] != NULL)
-    {
-        contentLength = strlen(argv[1]);
-    }
+/**
+ * read_func - reads from file_fro
+ * @file_from: where from
+ *
+ * Return: pointer to BUFFER
+ */
 
-    buffer = malloc((contentLength + 1) * sizeof(char));
-    if (buffer == NULL)
-    {
-        fprintf(stderr, "Error: Failed to allocate memory\n");
-        close(fileDescriptor);
-        return 99;
-    }
+char *read_func(const char *file_from)
+{
+	int fileDescriptor;
+	ssize_t bytesRead;
+	size_t contentLength;
+	char *buffer;
 
-    bytesRead = read(fileDescriptor, buffer, contentLength);
-    if (bytesRead == -1)
-    {
-        fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-        free(buffer);
-        close(fileDescriptor);
-        return 98;
-    }
-    buffer[bytesRead] = '\0';
+	fileDescriptor = open(file_from, O_RDONLY);
+	if (fileDescriptor == -1)
+	{
+		fprintf(stderr, "Error: Can't read from fiile %s", file_from);
+		return (98);
+	}
 
-    close(fileDescriptor);
+	contentLength = strlen(file_from);
+	buffer = malloc((contentLength + 1) * sizeof(char));
+	if (buffer == NULL)
+	{
+		fprintf(stderr, "Error: Failed to allocate memory \n");
+		close(fileDescriptor);
+		return (99);
+	}
 
-    fileDescriptor = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fileDescriptor == -1)
-    {
-        fprintf(stderr, "Error: Can't write to file %s\n", argv[2]);
-        free(buffer);
-        return 99;
-    }
+	bytesRead = read(fileDescriptor, buffer, contentLength);
+	if (bytesRead == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", file_from);
+		free(buffer);
+		close(fileDescriptor);
+		return (98);
+	}
+	buffer[bytesRead] = '\0';
+	close(fileDescriptor);
 
-    bytesWritten = write(fileDescriptor, buffer, bytesRead);
-    if (bytesWritten == -1)
-    {
-        fprintf(stderr, "Error: Can't write to file %s\n", argv[2]);
-        free(buffer);
-        close(fileDescriptor);
-        return 99;
-    }
+	return(buffer);
+}
 
-    close(fileDescriptor);
-    free(buffer);
+/**
+ * write_func - writes from buffer to file_to
+ * @file_to: name of file
+ *
+ * Return: bytesWritten
+ *
+ */
 
-    return 0;
+int write_func(const char *file_from, const char *file_to)
+{
+	int fileDescriptor;
+	ssize_t bytesWritten;
+	ssize_t content length;
+	char *buffer = read_func(file_from);
+
+	fileDescriptor = open(file_to, O_WRONLY | O_TRUNC | O_CREAT);
+	if (fileDescriptor == -1)
+	{
+		fprintf(stderr, "Error: Can't write to %s\n", file_to);
+		close(fileDescriptor);
+		return (99);
+	}
+	bytesWritten = write(fileDescriptor, buffer, contentLength);
+	if (bytesWritten == -1)
+	{
+		fprintf(stderr, "Error: Can't write to %s\n", file_to);
+		close(fileDecsriptor);
+		return (99);
+	}
+
+	close(fileDescriptor);
+
+	return (bytesWritten);
 }
