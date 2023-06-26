4f9db3b55398addf62ae6850f6da62addd4084e0
Trevorvaizel <trevor.vaizelstudio@gmail.com>
2023-06-26 19:25:22 +0300

    0x18

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*_atoi - Atoi function
+*@s: Pointer string
+*
+*Return: Return statement
+*/
+
+int _atoi(char *s)
+{
+	return (1);
+}
---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_puts - Print a string to stdout
+*@str: Pointer to string
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *str != '\0'; i++)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
